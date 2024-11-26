d8bb47622e5c497be5cf809777d115bc594d91b0
TonyNMS <j.li@ncl-marine.com>
2024-11-26 10:27:51 +0000

    remove the server port attrib

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 3001,},
+  
   base: "/Vessel_Scheduler/"
 })
