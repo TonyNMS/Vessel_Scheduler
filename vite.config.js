431c7712e03166f16c98b907267e862ef6c9890a
TonyNMS <j.li@ncl-marine.com>
2024-11-26 11:15:49 +0000

    Update package.json, vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Vessel_Scheduler/", 
   plugins: [react()],
   
-  base: "/Vessel_Scheduler/"
 })
